f1fa7b1ad941a81e5cf8e5eb3aec12fcf334c0f8
dylan <hcj_of@outlook.com>
2022-04-04 12:22:04 +0800

    style(eslint + prettier): 调整eslint设置和代码风格 并 重新格式化代码

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,26 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
-      env: {
-        jest: true,
-      },
-    },
-  ],
-};
+	env: {
+		browser: true,
+		es2021: true,
+		node: true
+	},
+	extends: ['plugin:vue/vue3-essential', 'standard', '@vue/typescript/recommended', 'plugin:prettier/recommended'],
+	parserOptions: {
+		ecmaVersion: 2020,
+		parser: '@typescript-eslint/parser',
+		sourceType: 'module'
+	},
+	plugins: ['vue', '@typescript-eslint'],
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+	},
+	overrides: [
+		{
+			files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+			env: {
+				jest: true
+			}
+		}
+	]
+}
